9e4d44aca37576949076b83e57ad6be5e1b73fe8
Tjoosten <Topairy@gmail.com>
2018-09-16 22:30:54 +0200

    Fix #5 Implement view composer docblock

--- app/Composers/LayoutComposer.php
+++ app/Composers/LayoutComposer.php
@@ -5,10 +5,18 @@ namespace Compass\Composers;
 use Illuminate\View\View;
 
 /**
- * @todo Docblocksx
+ * Class LayoutComposer
+ * 
+ * @package Compass\Composers
  */
 class LayoutComposer 
 {
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view The builder instance for the blade view.
+     * @return void
+     */
     public function compose(View $view): void
     {
         if (auth()->check()) {
